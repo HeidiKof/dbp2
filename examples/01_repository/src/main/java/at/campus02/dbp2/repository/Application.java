31f746e9ddb6a6903d9301a65e974f3bbad5f623
Heidemarie Kofler <heidemarie.kofler@edu.campus02.at>
2021-10-07 11:09:13 +0200

    init commit - copied examples

--- examples/01_repository/src/main/java/at/campus02/dbp2/repository/Application.java
+++ examples/01_repository/src/main/java/at/campus02/dbp2/repository/Application.java
@@ -8,5 +8,27 @@ public class Application {
 
     public static void main(String[] args) {
         log("application started");
+
+        CustomerRepository repository = new InMemoryRepository();
+
+        Customer customer = new Customer();
+        customer.setEmail("customer1@customers.com");
+        customer.setFirstname("Carlo");
+        customer.setLastname("Customer");
+
+        repository.create(customer);
+        log("Customer created: " + customer);
+
+        Customer fromRepository = repository.read(customer.getEmail());
+        log("Customer read: " + fromRepository);
+
+        fromRepository.setFirstname("Conrad");
+        repository.update(fromRepository);
+        Customer updated = repository.read(fromRepository.getEmail());
+        log("Customer updated " + updated);
+
+        repository.delete(updated);
+        Customer deleted = repository.read(updated.getEmail());
+        log("Customer deleted: " + deleted);
     }
 }
