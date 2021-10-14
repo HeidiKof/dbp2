385e781cfa277a1c46eb51514ec1eedf46b9624d
Heidemarie Kofler <heidemarie.kofler@edu.campus02.at>
2021-10-14 11:06:58 +0200

    Init Commit Create + Tests

---
+++ examples/02_mappings/src/test/java/MyVeryFirstTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyVeryFirstTest {
+
+    @Test
+    public void toUpperCaseConvertsAllLettersToUppercase() {
+        // given
+        String teststring = "kleinGROSS";
+
+        // when
+        String result = teststring.toUpperCase();
+
+        // then
+        Assertions.assertEquals("KLEINGROSS", result);
+    }
+}
